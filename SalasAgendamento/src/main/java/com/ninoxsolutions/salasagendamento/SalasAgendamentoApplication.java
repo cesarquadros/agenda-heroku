a1767259a93574773cf8a06a5e726c6ff920d748
Cesar <cesar.quadros@b2wdigital.com>
2019-11-25 11:15:11 -0300

    Primeiro commit - Estrutura do projeto

---
+++ SalasAgendamento/src/main/java/com/ninoxsolutions/salasagendamento/SalasAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.ninoxsolutions.salasagendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalasAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalasAgendamentoApplication.class, args);
+	}
+
+}
---
+++ SalasAgendamento/src/test/java/com/ninoxsolutions/salasagendamento/SalasAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninoxsolutions.salasagendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
